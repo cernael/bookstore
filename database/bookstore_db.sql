47890c3fb690e7621d816f4be15513b40f6f5cbe
cernael <tomas@cernael.com>
2013-12-03 14:31:11 +0100

    database update
    changed column names to all lower-case, added four books, two events,
    and requisites in other tables

--- database/bookstore_db.sql
+++ database/bookstore_db.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 02, 2013 at 02:38 PM
+-- Generation Time: Dec 03, 2013 at 02:29 PM
 -- Server version: 5.5.32
 -- PHP Version: 5.4.16
 
@@ -30,7 +30,7 @@ USE `bookstore_db`;
 
 CREATE TABLE IF NOT EXISTS `articles` (
   `article_id` int(11) NOT NULL AUTO_INCREMENT,
-  `ISBN` varchar(13) NOT NULL,
+  `isbn` varchar(13) NOT NULL,
   `title` varchar(100) NOT NULL,
   `publisher` varchar(100) NOT NULL,
   `publishing_year` varchar(20) NOT NULL,
@@ -39,15 +39,17 @@ CREATE TABLE IF NOT EXISTS `articles` (
   `language` varchar(30) NOT NULL,
   PRIMARY KEY (`article_id`),
   KEY `shelf_id` (`shelf_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=125 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=129 ;
 
 --
 -- Dumping data for table `articles`
 --
 
-INSERT INTO `articles` (`article_id`, `ISBN`, `title`, `publisher`, `publishing_year`, `shelf_id`, `inventory`, `language`) VALUES
-(123, '0987654321', 'Misery', 'Stephen King', '1985', 31, 2, 'Eng'),
-(124, '0984854321', 'Sagan om ringen', 'J.R:R Tolkien', '1989', 30, 4, 'SVE');
+INSERT INTO `articles` (`article_id`, `isbn`, `title`, `publisher`, `publishing_year`, `shelf_id`, `inventory`, `language`) VALUES
+(125, '9780596804374', 'PHP: The Good Parts', 'O''Reilly Media Inc.', '2010', 30, 1, 'English'),
+(126, '9781430249320', 'Beginning JQuery', 'Apress', '2013', 30, 1, 'English'),
+(127, '9781847194145', 'Object-Oriented JavaScript', 'Packt Publishing Ltd.', '2008', 30, 1, 'English'),
+(128, '9789188316165', 'In i musiken', 'Bo Ejeby Förlag', '1987', 31, 1, 'Svenska');
 
 -- --------------------------------------------------------
 
@@ -62,6 +64,16 @@ CREATE TABLE IF NOT EXISTS `article_category` (
   KEY `category_id` (`category_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `article_category`
+--
+
+INSERT INTO `article_category` (`article_id`, `category_id`) VALUES
+(125, 2),
+(126, 2),
+(127, 2),
+(128, 1);
+
 -- --------------------------------------------------------
 
 --
@@ -73,7 +85,17 @@ CREATE TABLE IF NOT EXISTS `authors` (
   `author_first_name` varchar(30) NOT NULL,
   `author_last_name` varchar(30) NOT NULL,
   PRIMARY KEY (`author_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Dumping data for table `authors`
+--
+
+INSERT INTO `authors` (`author_id`, `author_first_name`, `author_last_name`) VALUES
+(1, 'Peter B.', 'MacIntyre'),
+(2, 'Jack', 'Franklin'),
+(3, 'Stoyan', 'Stefanov'),
+(4, 'Peter', 'Bastian');
 
 -- --------------------------------------------------------
 
@@ -88,6 +110,16 @@ CREATE TABLE IF NOT EXISTS `book_author` (
   KEY `article_id` (`article_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `book_author`
+--
+
+INSERT INTO `book_author` (`author_id`, `article_id`) VALUES
+(1, 125),
+(2, 126),
+(3, 127),
+(4, 128);
+
 -- --------------------------------------------------------
 
 --
@@ -98,7 +130,15 @@ CREATE TABLE IF NOT EXISTS `categories` (
   `category_id` int(11) NOT NULL AUTO_INCREMENT,
   `category_name` varchar(30) NOT NULL,
   PRIMARY KEY (`category_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`category_id`, `category_name`) VALUES
+(1, 'music'),
+(2, 'programming');
 
 -- --------------------------------------------------------
 
@@ -111,7 +151,14 @@ CREATE TABLE IF NOT EXISTS `employees` (
   `employee_first_name` varchar(30) NOT NULL,
   `employee_last_namme` varchar(30) NOT NULL,
   PRIMARY KEY (`employee_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `employees`
+--
+
+INSERT INTO `employees` (`employee_id`, `employee_first_name`, `employee_last_namme`) VALUES
+(1, 'Karl Kristian', 'Kalashnikov');
 
 -- --------------------------------------------------------
 
@@ -126,7 +173,15 @@ CREATE TABLE IF NOT EXISTS `inventory_event` (
   `employee_id` int(11) NOT NULL,
   PRIMARY KEY (`event_id`),
   KEY `employee_id` (`employee_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `inventory_event`
+--
+
+INSERT INTO `inventory_event` (`event_id`, `type`, `date`, `employee_id`) VALUES
+(1, 'add', '2013-12-03 13:16:43', 1),
+(2, 'sale', '2013-12-03 13:17:58', 1);
 
 -- --------------------------------------------------------
 
@@ -140,8 +195,20 @@ CREATE TABLE IF NOT EXISTS `line_item` (
   `article_id` int(11) NOT NULL,
   `amount` int(11) NOT NULL,
   PRIMARY KEY (`item_id`),
-  KEY `event_id` (`event_id`,`article_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  KEY `event_id` (`event_id`),
+  KEY `article_id` (`article_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+--
+-- Dumping data for table `line_item`
+--
+
+INSERT INTO `line_item` (`item_id`, `event_id`, `article_id`, `amount`) VALUES
+(1, 1, 125, 1),
+(2, 1, 126, 1),
+(3, 1, 127, 1),
+(4, 1, 128, 2),
+(5, 2, 128, 1);
 
 -- --------------------------------------------------------
 
@@ -152,12 +219,23 @@ CREATE TABLE IF NOT EXISTS `line_item` (
 CREATE TABLE IF NOT EXISTS `prices` (
   `price_id` int(11) NOT NULL AUTO_INCREMENT,
   `article_id` int(11) NOT NULL,
-  `F_pris` decimal(7,2) NOT NULL,
+  `f_pris` decimal(7,2) NOT NULL,
   `price` decimal(7,2) NOT NULL,
   `date` date NOT NULL,
   PRIMARY KEY (`price_id`),
   KEY `article_id` (`article_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+--
+-- Dumping data for table `prices`
+--
+
+INSERT INTO `prices` (`price_id`, `article_id`, `f_pris`, `price`, `date`) VALUES
+(1, 125, '170.00', '249.00', '2013-12-02'),
+(2, 126, '150.00', '219.00', '2013-12-03'),
+(3, 127, '200.00', '299.00', '2013-12-03'),
+(4, 128, '50.00', '69.00', '2013-12-03'),
+(5, 125, '150.00', '159.00', '2013-12-03');
 
 -- --------------------------------------------------------
 
@@ -211,6 +289,13 @@ ALTER TABLE `book_author`
 --
 ALTER TABLE `inventory_event`
   ADD CONSTRAINT `employeelink` FOREIGN KEY (`employee_id`) REFERENCES `employees` (`employee_id`);
+
+--
+-- Constraints for table `line_item`
+--
+ALTER TABLE `line_item`
+  ADD CONSTRAINT `line_item` FOREIGN KEY (`article_id`) REFERENCES `articles` (`article_id`),
+  ADD CONSTRAINT `eventlink` FOREIGN KEY (`event_id`) REFERENCES `inventory_event` (`event_id`);
 
 --
 -- Constraints for table `prices`
